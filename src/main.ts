8a20620b9bbe7e16512f8d7049213e8eceb549d1
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2023-07-21 23:08:28 -0600

    created VERY basic drawing program

---
+++ src/main.ts
@@ -0,0 +1,64 @@
+window.addEventListener("DOMContentLoaded", () => {});
+
+let canvas: HTMLCanvasElement | null;
+
+function setCanvasContextSize(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+	ctx.canvas.width = canvas.getBoundingClientRect().width - 8;
+	ctx.canvas.height = canvas.getBoundingClientRect().height - 8;
+}
+
+function drawFromPointer(path: Path2D, ctx: CanvasRenderingContext2D, event: MouseEvent, lastPoint: { x: number; y: number }) {
+	path.moveTo(lastPoint.x, lastPoint.y);
+	path.lineTo(event.offsetX, event.offsetY);
+
+	ctx.strokeStyle = "grey";
+	ctx.stroke(path);
+
+	lastPoint.x = event.offsetX;
+	lastPoint.y = event.offsetY;
+}
+
+function drawBackground() {}
+
+window.addEventListener("DOMContentLoaded", () => {
+	canvas = <HTMLCanvasElement>document.getElementById("canvas");
+	let ctx = canvas!.getContext("2d")!;
+
+	let path = new Path2D();
+	let lastPoint = { x: 0, y: 0 };
+
+	setCanvasContextSize(ctx, canvas!);
+	window.addEventListener("resize", () => {
+		setCanvasContextSize(ctx, canvas!);
+
+		ctx.strokeStyle = "grey";
+		ctx.stroke(path);
+	});
+
+	canvas.addEventListener("mouseenter", (event) => {
+		lastPoint.x = event.offsetX;
+		lastPoint.y = event.offsetY;
+	});
+
+	let mouseMoveCallback = (event: MouseEvent) => {
+		path.moveTo(lastPoint.x, lastPoint.y);
+		path.lineTo(event.offsetX, event.offsetY);
+
+		ctx.strokeStyle = "grey";
+		ctx.stroke(path);
+
+		lastPoint.x = event.offsetX;
+		lastPoint.y = event.offsetY;
+	};
+
+	canvas.addEventListener("mousedown", (event) => {
+		lastPoint.x = event.offsetX;
+		lastPoint.y = event.offsetY;
+
+		canvas!.addEventListener("mousemove", mouseMoveCallback);
+	});
+
+	canvas.addEventListener("mouseup", () => {
+		canvas?.removeEventListener("mousemove", mouseMoveCallback);
+	});
+});
